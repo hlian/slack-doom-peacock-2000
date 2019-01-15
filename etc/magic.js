3dd9868466db06bf7732b14c01d2a94c1180d05c
Hao Lian <hi@haolian.org>
2019-01-15 17:36:36 -0500

    sigh

--- etc/magic.js
+++ etc/magic.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   $.ajax({
-    url: 'https://raw.githubusercontent.com/hlian/slack-night-mode/master/css/raw/variants/solarized-dark.css',
+    url: 'https://raw.githubusercontent.com/hlian/slack-night-mode/master/css/variants/solarized-dark.css',
     success: function(css) {
       $("<style></style>").appendTo('head').html(css);
     }
