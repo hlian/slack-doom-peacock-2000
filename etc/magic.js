ae146c4110fc4390675a8d9dbcf3ec65b07602f2
Hao Lian <hi@haolian.org>
2019-05-14 14:27:27 -0400

    error

--- etc/magic.js
+++ etc/magic.js
@@ -3,6 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
     url: 'https://raw.githubusercontent.com/hlian/slack-night-mode/master/css/variants/solarized-dark.css',
     success: function(css) {
       $("<style></style>").appendTo('head').html(css);
+    },
+    error: function(e) {
+      alert(JSON.stringify(e))
     }
   });
 });
